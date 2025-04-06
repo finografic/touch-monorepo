479fb0c07960425af2bb671d66d0cf1abad02c25
Justin <justin.blair.rankin@gmail.com>
2025-04-06 21:47:27 +0200

    NEW: Providers

--- apps/client/src/layout/Layout.tsx
+++ apps/client/src/layout/Layout.tsx
@@ -1,15 +1,18 @@
 import { Outlet } from 'react-router-dom';
 import { styles } from './Layout.styles';
 import { OrdersProvider } from '../providers/OrdersProvider';
+import { PaginationProvider } from '../providers/PaginationProvider/PaginationProvider';
 
 export const Layout = () => {
   return (
     <OrdersProvider>
-      <div css={styles}>
-        <main>
-          <Outlet />
-        </main>
-      </div>
+      <PaginationProvider>
+        <div css={styles}>
+          <main>
+            <Outlet />
+          </main>
+        </div>
+      </PaginationProvider>
     </OrdersProvider>
   );
 };
