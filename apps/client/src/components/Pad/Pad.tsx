e8a20b9e8a32a29f4aec010c82a69f3346fb51a7
Justin <justin.blair.rankin@gmail.com>
2025-04-07 00:11:06 +0200

    main menu, improved context

--- apps/client/src/components/Pad/Pad.tsx
+++ apps/client/src/components/Pad/Pad.tsx
@@ -3,29 +3,16 @@ import { OrderItem } from 'types/orders.types';
 
 type PadProps = {
   number: number;
-  isFirst?: boolean;
-  isError?: boolean;
-  isSpecial?: boolean;
 };
 
-export const Pad = ({ number, isFirst, isError, isSpecial }: PadProps) => {
+export const Pad = ({ number }: PadProps) => {
   const { togglePad, orders } = useOrders();
-  const order = orders.find((order) => order.itemNumber === number) as OrderItem;
 
-  const className = [
-    'pad',
-    isFirst && 'first',
-    isError && 'error',
-    isSpecial && 'special',
-    order?.isSelected && 'active',
-  ]
-    .filter(Boolean)
-    .join(' ');
+  const order = orders.find((order) => order.itemNumber === number) as OrderItem;
+  const className = ['pad', order?.isSelected && 'active'].filter(Boolean).join(' ');
 
   const handleClick = () => {
-    if (!isFirst && !isError && !isSpecial) {
-      togglePad(number);
-    }
+    togglePad(number);
   };
 
   return <div className={className} onClick={handleClick} />;
