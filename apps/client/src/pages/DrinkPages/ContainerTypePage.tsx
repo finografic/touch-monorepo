46b0cadbc23ecdf7b28d318c1a7257e271432787
Justin <justin.blair.rankin@gmail.com>
2025-04-21 10:31:37 +0200

    EOD - container-types FETCHED

--- apps/client/src/pages/DrinkPages/ContainerTypePage.tsx
+++ apps/client/src/pages/DrinkPages/ContainerTypePage.tsx
@@ -1,10 +1,10 @@
 import { stylesItemsGrid } from './grid.styles';
 import type { ContainerType } from 'types/orders.types';
-import { CONTAINER_TYPES } from './data/container-type.data';
 import { useOrderSelection, OrderFieldKeys } from 'hooks/useOrderSelection';
 import { usePagination } from 'providers/PaginationProvider/PaginationContext';
 import { useEffect } from 'react';
 import { getGridFlowClasses } from './utils/getGridFlowClasses';
+import { useGetContainerTypes } from 'queries/container-types';
 
 export const ContainerTypePage = () => {
   const {
@@ -16,16 +16,25 @@ export const ContainerTypePage = () => {
   });
 
   const { setIsNextDisabled } = usePagination();
+  const { data: containerTypes, isLoading, error } = useGetContainerTypes();
 
   // Update next button state based on selection
   useEffect(() => {
     setIsNextDisabled(!hasValidSelection);
   }, [hasValidSelection, setIsNextDisabled]);
 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error || !containerTypes) {
+    return <div>Error loading container types</div>;
+  }
+
   return (
     <section css={stylesItemsGrid}>
-      <div className={getGridFlowClasses(CONTAINER_TYPES.length)}>
-        {CONTAINER_TYPES.map((containerType) => (
+      <div className={getGridFlowClasses(containerTypes.length)}>
+        {containerTypes.map((containerType: ContainerType) => (
           <div
             key={containerType.id}
             className={`item-button ${selectedContainer?.id === containerType.id ? 'selected' : ''}`}
--- apps/client/src/queries/container-types/index.ts
+++ apps/client/src/queries/container-types/index.ts
@@ -1,1 +1,3 @@
 export const GET_CONTAINER_TYPES_QUERYKEY = ['get-container-types'] as const;
+export * from './useGetContainerTypes';
+export * from './useGetContainerType';
