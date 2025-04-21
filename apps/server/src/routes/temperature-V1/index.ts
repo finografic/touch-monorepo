00b22504769f2245da0e9e4ea5e5aec073ef6965
Justin <justin.blair.rankin@gmail.com>
2025-04-21 09:42:44 +0200

    EOD - before FIXING DYNAMIC CONFIGS

--- apps/server/src/routes/temperature-V1/index.ts
+++
@@ -1,7 +0,0 @@
-import { createRouter } from 'lib/create-app';
-import * as handlers from './temperature.handlers';
-import * as routes from './temperature.routes';
-
-export default createRouter()
-  .openapi(routes.calculateRoute, handlers.calculate)
-  .openapi(routes.getSettings, handlers.getSettings);
