434642c0746a28bee62105063a5eed8537c99eb3
Justin <justin.blair.rankin@gmail.com>
2025-05-03 05:25:36 +0200

    improving - UI from loaderData

--- apps/client/src/layout/Layout.tsx
+++ apps/client/src/layout/Layout.tsx
@@ -14,15 +14,16 @@ import { Loader } from '../components/Loader/Loader';
 // import { DataLayer } from 'layout/DataLayer';
 import { DevProvider } from 'providers/DevProvider/DevProvider';
 import { LayoutUiProvider } from 'providers/LayoutUiProvider/LayoutUiProvider';
+import { useRouteConfig } from 'routes/hooks/useRouteConfig';
 
 // export const Layout: FC<{ children: ReactNode }> = ({ children }) => {
 export const Layout: FC = () => {
-  // const { route, fieldKey } = useRouteConfig();
+  const { route, fieldKey } = useRouteConfig();
   // const data = useLoaderData();
   const isMounted: boolean = !!useIsMounted();
 
-  // const loaderData = useRouteLoaderData(fieldKey || 'root');
-  // log('LOADER_DATA - route', 'cyan', loaderData);
+  const loaderData = useRouteLoaderData(fieldKey || 'root');
+  log('LOADER_DATA - route', 'cyan', loaderData);
 
   if (!isMounted) {
     return <Loader message="Loading..." />;
