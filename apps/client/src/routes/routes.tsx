4c25a103e1a148abd9b4adfa12abd9fc8fbe5d9d
Justin <justin.blair.rankin@gmail.com>
2025-05-25 16:43:58 +0200

    temperature wip..

--- apps/client/src/routes/routes.tsx
+++ apps/client/src/routes/routes.tsx
@@ -63,7 +63,6 @@ export const routes: RouteObject[] = [
         loader: LoaderDataHelper[OrderFieldKeys.drinkSubtype],
         element: <GenericSelectPage />,
       },
-
       // ------------------------------------------------------------------------ //
       {
         path: PATHS.drinkVolume,
