4326ac9bf99efacc8905d5583bd7985fd72675c7
Justin <justin.blair.rankin@gmail.com>
2025-07-05 14:27:26 +0200

    USE NEW READABLE_VIEW

--- apps/client/src/dev-tools/DevFilterResults/DevFilterResults.tsx
+++ apps/client/src/dev-tools/DevFilterResults/DevFilterResults.tsx
@@ -1,44 +1,41 @@
 import { useFilters } from 'hooks/useFilters';
 import { styles } from './DevFilterResults.styles';
-import { Col, Row } from 'react-grid-system';
 
 export const DevFilterResults = () => {
   const { dataFiltered, filters } = useFilters({});
 
   return (
     <div id="dev-filter-results" css={styles}>
-      <Row>
-        <Col xs={12} className="filters">
-          <h4>Filters ({Object.keys(filters).length}):</h4>
-          <pre>{JSON.stringify(filters, null, 2)}</pre>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={12} className="results-list">
-          <h4>Results: {dataFiltered.length}</h4>
-          <pre>
-            {dataFiltered.map((item: any) => (
-              <Row key={item.id} direction="row" align="center" className="result-row">
-                <Col xs={3}>
-                  <strong>{item.drinkTypeName}</strong>
-                </Col>
-                <Col xs={2}>
-                  <p>{item.drinkSubtypeName}</p>
-                </Col>
-                <Col xs={2}>
-                  <p>{item.volumeName}</p>
-                </Col>
-                <Col xs={2}>
-                  <p>{item.containerTypeName}</p>
-                </Col>
-                <Col xs={3}>
-                  <p>{item.temperatureProfileId}</p>
-                </Col>
-              </Row>
-            ))}
-          </pre>
-        </Col>
-      </Row>
+      <div className="filters">
+        <h4>Filters ({Object.keys(filters).length}):</h4>
+        <pre>{JSON.stringify(filters, null, 2)}</pre>
+      </div>
+      <div className="results-list">
+        <h4>Results: {dataFiltered.length}</h4>
+        {dataFiltered.map((item: any) => (
+          <div
+            key={item.id}
+            className="result-row"
+            style={{ display: 'flex', alignItems: 'center', marginBottom: '8px' }}
+          >
+            <div style={{ flex: '3', marginRight: '8px' }}>
+              <strong>{item.drinkType}</strong>
+            </div>
+            <div style={{ flex: '2', marginRight: '8px' }}>
+              <p style={{ margin: 0 }}>{item.drinkSubtype}</p>
+            </div>
+            <div style={{ flex: '2', marginRight: '8px' }}>
+              <p style={{ margin: 0 }}>{item.volume}</p>
+            </div>
+            <div style={{ flex: '2', marginRight: '8px' }}>
+              <p style={{ margin: 0 }}>{item.containerType}</p>
+            </div>
+            <div style={{ flex: '3' }}>
+              <p style={{ margin: 0 }}>{item.temperatureProfile}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
