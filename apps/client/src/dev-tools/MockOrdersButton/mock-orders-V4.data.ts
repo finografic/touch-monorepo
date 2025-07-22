bc94777f000411f099572fca10fa0d869e637485
Justin <justin.blair.rankin@gmail.com>
2025-07-23 00:40:40 +0200

    final fixes.. wip.. TemperaturePage

---
+++ apps/client/src/dev-tools/MockOrdersButton/mock-orders-V4.data.ts
@@ -0,0 +1,150 @@
+import type { OrderItem } from 'types/orders.types';
+import { ItemType } from 'types/orders.types';
+
+export const MOCK_ORDERS_DATA: OrderItem[] = [
+  {
+    id: 'cmd0wgv4n001frxlwenfiglb4',
+    itemType: ItemType.A,
+    itemNumber: 0,
+    isSelected: true,
+    filters: {
+      drinkType: {
+        id: 'cmd0wgs960001mklwuhnicrqt',
+        name: 'vino',
+        hasSubtypes: true,
+        defaultTempConsume: 15,
+        lookup: { drinkType: 'vino' },
+      },
+      drinkSubtype: {
+        id: 'cmd0wgs980009mklwwjczy7qc',
+        name: 'tinto',
+        defaultTempConsume: 15,
+        lookup: { drinkSubtype: 'tinto' },
+      },
+      drinkVolume: { id: 'cmd0wgtyu0005pxlwbsp7we8g', name: '50cl', lookup: { volume: '50cl' } },
+      containerType: {
+        id: 'cmd0wgtdy0001oslwwbiv2yei',
+        name: 'vidrio',
+        lookup: { containerType: 'vidrio' },
+      },
+    },
+    process: { status: 'idle' },
+    ids: ['cmd0wgv4n001frxlwenfiglb4'],
+  },
+  {
+    id: 'cmd0wgv4n001frxlwenfiglb4',
+    itemType: ItemType.B,
+    itemNumber: 1,
+    isSelected: false,
+    filters: {
+      drinkType: {
+        id: 'cmd0wgs960001mklwuhnicrqt',
+        name: 'vino',
+        hasSubtypes: true,
+        defaultTempConsume: 15,
+        lookup: { drinkType: 'vino' },
+      },
+      drinkSubtype: {
+        id: 'cmd0wgs980009mklwwjczy7qc',
+        name: 'tinto',
+        defaultTempConsume: 15,
+        lookup: { drinkSubtype: 'tinto' },
+      },
+      drinkVolume: { id: 'cmd0wgtyu0005pxlwbsp7we8g', name: '50cl', lookup: { volume: '50cl' } },
+      containerType: {
+        id: 'cmd0wgtdy0001oslwwbiv2yei',
+        name: 'vidrio',
+        lookup: { containerType: 'vidrio' },
+      },
+    },
+    process: { status: 'idle' },
+    ids: ['cmd0wgv4n001frxlwenfiglb4'],
+  },
+  {
+    id: 'cmd0wgv4n001frxlwenfiglb4',
+    itemType: ItemType.B,
+    itemNumber: 2,
+    isSelected: true,
+    filters: {
+      drinkType: {
+        id: 'cmd0wgs960001mklwuhnicrqt',
+        name: 'vino',
+        hasSubtypes: true,
+        defaultTempConsume: 15,
+        lookup: { drinkType: 'vino' },
+      },
+      drinkSubtype: {
+        id: 'cmd0wgs980009mklwwjczy7qc',
+        name: 'tinto',
+        defaultTempConsume: 15,
+        lookup: { drinkSubtype: 'tinto' },
+      },
+      drinkVolume: { id: 'cmd0wgtyu0005pxlwbsp7we8g', name: '50cl', lookup: { volume: '50cl' } },
+      containerType: {
+        id: 'cmd0wgtdy0001oslwwbiv2yei',
+        name: 'vidrio',
+        lookup: { containerType: 'vidrio' },
+      },
+    },
+    process: { status: 'idle' },
+    ids: ['cmd0wgv4n001frxlwenfiglb4'],
+  },
+  {
+    id: 'cmd0wgv4n001frxlwenfiglb4',
+    itemType: ItemType.B,
+    itemNumber: 5,
+    isSelected: true,
+    filters: {
+      drinkType: {
+        id: 'cmd0wgs960001mklwuhnicrqt',
+        name: 'vino',
+        hasSubtypes: true,
+        defaultTempConsume: 15,
+        lookup: { drinkType: 'vino' },
+      },
+      drinkSubtype: {
+        id: 'cmd0wgs980009mklwwjczy7qc',
+        name: 'tinto',
+        defaultTempConsume: 15,
+        lookup: { drinkSubtype: 'tinto' },
+      },
+      drinkVolume: { id: 'cmd0wgtyu0005pxlwbsp7we8g', name: '50cl', lookup: { volume: '50cl' } },
+      containerType: {
+        id: 'cmd0wgtdy0001oslwwbiv2yei',
+        name: 'vidrio',
+        lookup: { containerType: 'vidrio' },
+      },
+    },
+    process: { status: 'idle' },
+    ids: ['cmd0wgv4n001frxlwenfiglb4'],
+  },
+  {
+    id: 'cmd0wgv4n001frxlwenfiglb4',
+    itemType: ItemType.C,
+    itemNumber: 9,
+    isSelected: true,
+    filters: {
+      drinkType: {
+        id: 'cmd0wgs960001mklwuhnicrqt',
+        name: 'vino',
+        hasSubtypes: true,
+        defaultTempConsume: 15,
+        lookup: { drinkType: 'vino' },
+      },
+      drinkSubtype: {
+        id: 'cmd0wgs980009mklwwjczy7qc',
+        name: 'tinto',
+        defaultTempConsume: 15,
+        lookup: { drinkSubtype: 'tinto' },
+      },
+      drinkVolume: { id: 'cmd0wgtyu0005pxlwbsp7we8g', name: '50cl', lookup: { volume: '50cl' } },
+      containerType: {
+        id: 'cmd0wgtdy0001oslwwbiv2yei',
+        name: 'vidrio',
+        lookup: { containerType: 'vidrio' },
+      },
+    },
+    process: { status: 'idle' },
+    ids: ['cmd0wgv4n001frxlwenfiglb4'],
+  },
+];
