c85be0d108b1da27745ee4833ca3c63fab57cf6c
Justin <justin.blair.rankin@gmail.com>
2025-08-31 17:38:03 +0200

    DEPLOYMENT TO LOCAL - DONE, WORKING !!

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -4,6 +4,7 @@ import chalk from 'chalk';
 import app from './app';
 import { env } from './env.server';
 
+console.log('🚀 App Ready:', chalk.cyan('http://localhost:3000'));
 console.log('🚀 API Listening:', chalk.cyan(`${env.API_PROTOCOL}://${env.API_HOST}:${env.API_PORT}`));
 
 serve({
