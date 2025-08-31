5b0ff948488a26f333cac655037ab717f7c02af9
Justin <justin.blair.rankin@gmail.com>
2025-08-31 19:18:34 +0200

    bugs wip.. titles

--- apps/client/src/components/PageHeader/PageHeader.styles.ts
+++ apps/client/src/components/PageHeader/PageHeader.styles.ts
@@ -3,10 +3,10 @@ import { colors } from 'styles';
 
 export const styles = css`
   margin-bottom: 0rem;
-  transform: translateY(-10%);
+  transform: translateY(-5%);
 
   .page-title {
-    font-size: 2rem;
+    font-size: 1.5rem;
     font-weight: 500;
     line-height: 2;
     color: ${colors.textLight};
@@ -14,7 +14,7 @@ export const styles = css`
   }
 
   .page-subtitle {
-    font-size: 1.33rem;
+    font-size: 1.2rem;
     color: ${colors.text};
     margin: 0;
   }
