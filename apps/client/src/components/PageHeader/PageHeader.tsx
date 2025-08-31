d243dec10fe4176165663a8c5f4e0d0924e00ba4
Justin <justin.blair.rankin@gmail.com>
2025-09-01 00:45:30 +0200

    i18n updates..

--- apps/client/src/components/PageHeader/PageHeader.tsx
+++ apps/client/src/components/PageHeader/PageHeader.tsx
@@ -25,11 +25,9 @@ export const PageHeader = () => {
       const translationKey = `app.pages.${route.id}.title`;
       const translatedTitle = t(translationKey);
 
-      log('__DEV: ROUTE', 'cyan', route);
-
-      log('__DEV: TITLE_1 - id', 'orange', route.id);
-      log('__DEV: TITLE_2 - translationKey', 'orange', translationKey);
-      log('__DEV: TITLE_3 - translatedTitle', 'orange', translatedTitle);
+      log('__DEV: route', 'cyan', route);
+      log('__DEV: title_1 - id, translationKey', 'grey', route.id, translationKey);
+      log('__DEV: title_2 - translatedTitle', 'grey', translatedTitle);
 
       // Only use translation if it's not the same as the key (meaning translation exists)
       if (translatedTitle !== translationKey) {
