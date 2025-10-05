7bc95873058b6c5dae9cad65fc01effe3c663085
Justin <justin.blair.rankin@gmail.com>
2025-10-05 02:04:43 +0200

    REFACTOR - useFilters!!

--- apps/client/src/providers/FiltersProvider/index.ts
+++ apps/client/src/providers/FiltersProvider/index.ts
@@ -1,3 +1,4 @@
 export { FiltersContext, useFiltersContext } from './FiltersContext';
+export { useFilters } from './useFilters';
 export { FiltersProvider } from './FiltersProvider';
 export type { FiltersProviderProps, FiltersStore, FiltersValues } from './FiltersContext.types';
