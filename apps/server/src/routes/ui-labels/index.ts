d41f3a1ba54836e330a93dae17d28246ae0a2c96
Justin <justin.blair.rankin@gmail.com>
2025-11-24 12:21:06 +0100

    DataTable improved..

--- apps/server/src/routes/ui-labels/index.ts
+++ apps/server/src/routes/ui-labels/index.ts
@@ -1,8 +1,7 @@
 import { createRouter } from 'lib/create-app';
 // Use file manager approach instead of SDK import/export to avoid file merging issues
-import * as handlers from './ui-labels.handlers.inlang';
+// import * as handlers from './ui-labels.handlers.inlang';
+import * as handlers from './ui-labels.handlers';
 import * as routes from './ui-labels.routes';
 
-export default createRouter()
-  .openapi(routes.list, handlers.list)
-  .openapi(routes.save, handlers.save);
+export default createRouter().openapi(routes.list, handlers.list).openapi(routes.save, handlers.save);
