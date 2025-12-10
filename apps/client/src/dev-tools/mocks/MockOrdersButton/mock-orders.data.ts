31274906bd6c04ad6e9d71601223ad6111545f4b
Justin <justin.blair.rankin@gmail.com>
2025-12-10 06:28:10 +0100

    MOCK DATA FIX

--- apps/client/src/dev-tools/mocks/MockOrdersButton/mock-orders.data.ts
+++ apps/client/src/dev-tools/mocks/MockOrdersButton/mock-orders.data.ts
@@ -1,30 +1,29 @@
 import type { SlotMeta } from 'pages/MainPage/MainPage.types';
-
 import type { OrderFilters } from 'types/filters.types';
 import { SlotType } from 'types/slots.types';
 
 export const MOCK_ORDERS_DATA: OrderFilters = {
   mode: {
-    id: 'cmid93kry0002k67nzbp15fas',
+    id: 'cmizjus4600021y7nz9kngei9',
     name: '3',
   },
   drinkType: {
-    id: 'cmid93hj30001cq7njlsiwvt8',
+    id: 'cmizjuot50001ug7nt9u6por0',
     name: 'vino',
     hasSubtypes: true,
     defaultTempConsume: 15,
   },
   drinkSubtype: {
-    id: 'cmid93hj40009cq7ny6a6ds21',
-    name: 'tinto',
+    id: 'cmizjuot70009ug7ne131tvu1',
+    name: 'vino--tinto',
     defaultTempConsume: 15,
   },
   drinkVolume: {
-    id: 'cmid93jyn0006ie7nu0c4efck',
-    name: '33cl',
+    id: 'cmizjura20004097n87slmkf0',
+    name: '75cl',
   },
   containerType: {
-    id: 'cmid93j5a0001ga7nvk2iqpkq',
+    id: 'cmizjuqg00001yg7n8brno122',
     name: 'vidrio',
   },
 };
