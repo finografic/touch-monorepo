9b70874431385e6b08e81a69ef47b1c27dafd350
Justin <justin.blair.rankin@gmail.com>
2025-12-23 00:15:49 +0100

    fix: translations cleanup..

--- packages/i18n/src/types/index.ts
+++ packages/i18n/src/types/index.ts
@@ -1,8 +1,3 @@
-// Language types
 export type { LanguageConfig, LanguageInfo } from './language.types';
-
-// Type generation types
 export type { TypeGenerationConfig } from './type-generation.types';
-
-// Main i18n config
 export type { I18nConfig } from './i18n.types';
