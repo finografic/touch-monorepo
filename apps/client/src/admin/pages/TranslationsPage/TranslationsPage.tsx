4aea865fdb4044142c68587fbf46950b4da80bef
Justin <justin.blair.rankin@gmail.com>
2025-12-23 21:45:32 +0100

    fix: translations cleanup..

--- apps/client/src/admin/pages/TranslationsPage/TranslationsPage.tsx
+++ apps/client/src/admin/pages/TranslationsPage/TranslationsPage.tsx
@@ -14,20 +14,13 @@ import { styles } from './TranslationsPage.styles';
 import { useParams } from 'react-router-dom';
 import { translations } from '@workspace/i18n';
 import { flattenTranslations } from 'utils/flatten-translations';
-import { camelCase } from 'utils/string-case.utils';
 
 export const TranslationsPage: React.FC = () => {
   const { t } = useTranslation();
   const { domain } = useParams<{ domain: I18nTranslationsDomain }>();
   const pageTitleKey = `admin.pages.translations.domains.${domain}.title`;
-  // Extract groups, convert kebab-case to camelCase, and deduplicate
-  const groups = [
-    ...new Set(
-      flattenTranslations(domain, translations)
-        .map(({ key }) => key.split('.')[1])
-        .map((group) => camelCase(group)),
-    ),
-  ];
+  // Extract groups and deduplicate
+  const groups = [...new Set(flattenTranslations(domain, translations).map(({ key }) => key.split('.')[1]))];
   const [activeTab, setActiveTab] = useState<I18nDomainGroupKey>();
 
   log('translations:', 'blue', translations);
