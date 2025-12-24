2b242d3c23999523cf57e3589e28d75636e9bfe9
Justin <justin.blair.rankin@gmail.com>
2025-12-24 11:40:27 +0100

    fix: translations cleanup..

--- apps/client/src/admin/pages/TranslationsPage/TranslationsTable/TranslationsTable.tsx
+++ apps/client/src/admin/pages/TranslationsPage/TranslationsTable/TranslationsTable.tsx
@@ -4,7 +4,7 @@ import { useDebouncedCallback } from 'use-debounce';
 import { Flex } from '@radix-ui/themes';
 import createCuid from '@bugsnag/cuid';
 import { TranslationsRow } from './components/TranslationsRow';
-import { TableFormButtons } from '../TableFormButtons/TableFormButtons';
+import { TableFormButtons } from '../../TranslationsSHARED/TableFormButtons/TableFormButtons';
 import { styles } from '../../TranslationsSHARED/TranslationsTable.styles';
 import { useTranslationsTableForm } from './hooks/useTranslationsTableForm';
 import { useTranslationsTableHandlers } from './hooks/useTranslationsTableHandlers';
