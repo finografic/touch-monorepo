2d5528c64e5de6c90b483d0d2a04cb3a843718fa
Justin <justin.blair.rankin@gmail.com>
2025-12-28 01:10:43 +0100

    wip: placing translation keys..

--- apps/client/src/pages/TemperaturePage/index.ts
+++ apps/client/src/pages/TemperaturePage/index.ts
@@ -1,3 +1,3 @@
 export { TemperatureForm } from './TemperatureForm';
 export { TemperaturePage } from './TemperaturePage';
-export { useTemperatureManagement } from './useTemperatureFormAndFilter';
+export { useTemperatureFormAndFilter } from './useTemperatureFormAndFilter';
